f57d1aceffc8a4145552b008a33799236b0cf4f8
diarmidgrayne <108220580+diarmidgrayne@users.noreply.github.com>
2023-04-14 18:37:01 +0600

    Add files via upload

--- FirstApplication.java
+++ FirstApplication.java
@@ -1,11 +1,8 @@
-package org.talks;
+package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
         System.out.println("Hello, World!");
+        // Write your code here!
     }
 }
-
-
-
